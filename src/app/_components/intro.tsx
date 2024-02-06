62e83a1f7c882e6e1603361c43ecd3da49a6ea7a
Rohan Sharma <rohan@indotechmeter.com>
2024-02-06 09:00:39 +0530

    tips

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,7 +4,7 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
-        EnduroCo Knowledge Base
+         Knowledge Base
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         Deep dive into the science of endurance sports.
