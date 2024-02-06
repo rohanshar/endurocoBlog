2a44d01b41c2f8c00d28e2041bb64f89fdbc6544
Rohan Sharma <rohan@indotechmeter.com>
2024-02-06 11:37:09 +0530

    ui

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -7,23 +7,27 @@ type Props = {
 
 export function MoreStories({ posts }: Props) {
   return (
-    <section className="px-4 py-8">
+    <section className="px-4 py-8 bg-gray-100">
       <div className="max-w-screen-xl mx-auto">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-x-6 lg:gap-x-12 gap-y-12 md:gap-y-16 mb-32">
           {posts.map((post) => (
-            <PostPreview
-              key={post.slug}
-              title={post.title}
-              coverImage={post.coverImage}
-              date={post.date}
-              author={post.author}
-              slug={post.slug}
-              excerpt={post.excerpt}
-            />
+            <div className="border border-gray-300 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out p-4">
+              <PostPreview
+                key={post.slug}
+                title={post.title}
+                coverImage={post.coverImage}
+                date={post.date}
+                author={post.author}
+                slug={post.slug}
+                excerpt={post.excerpt}
+              />
+            </div>
           ))}
         </div>
       </div>
     </section>
   );
 }
+
+
 
